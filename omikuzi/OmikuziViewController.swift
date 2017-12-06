af97dcf61043a01da5eefec25062054e49f727a2
AMI <a_yamamoto@innovation.co.jp>
2017-12-06 12:23:09 +0900

    git管理のため

--- omikuzi/OmikuziViewController.swift
+++ omikuzi/OmikuziViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class OmikuziViewController: UIViewController {
     //前のページから名前を受け取る
-    var name:String? = "あみ"
+    var name:String? = ""
+    var result:[String] = ["大吉", "中吉", "小吉", "凶"]
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let name = name {
+            nameLabel.text = name + "さん"
+        }
+        //ランダムな数値0〜3
+        let num = Int(arc4random_uniform(4))
+        resultLabel.text = "\(result[num])です！"
     }
 
     override func didReceiveMemoryWarning() {
--- omikuzi/ViewController.swift
+++ omikuzi/ViewController.swift
@@ -9,10 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    
+    //前の画面から戻った時に、フィールドの値を初期化する
+    @IBAction func restart(_ segue: UIStoryboardSegue) {
+        nameField.text = ""
+        ageField.text = ""
+    }
+    
+    //名前のテキストフィールドに文字が入力されるたびにユーザデフォルトに保存する
+    @IBAction func nameFieldAction(_ sender: Any) {
+        let ud = UserDefaults.standard
+        if let name = nameField.text {
+            ud.set(name, forKey: "name")
+            ud.synchronize()
+        }
+    }
+    
+    //年齢のテキストフィールドに文字が入力されるたびにユーザデフォルトに保存する
+    @IBAction func ageFieldAction(_ sender: Any) {
+        let ud = UserDefaults.standard
+        if let age = ageField.text {
+            ud.set(age, forKey: "age")
+            ud.synchronize()
+        }
+    }
+    
+    
+    //次の画面に名前を渡す処理
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let omikuziController = segue.destination as! OmikuziViewController
+        
+        //名前を渡す
+        if let name = nameField.text {
+            omikuziController.name = name
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let ud = UserDefaults.standard
+        let name = ud.string(forKey: "name")
+        if let name = name {
+            nameField.text = name
+        }
+        let age = ud.string(forKey: "age")
+        if let age = age {
+            ageField.text = age
+        }
     }
 
     override func didReceiveMemoryWarning() {
